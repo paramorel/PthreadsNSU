258f6ce960c308b52b419c3e390f851bf4c5fb44
Anna Sendzyak <a.sendzyak@g.nsu.ru>
2019-10-31 16:04:54 +0700

    add second solution

--- 9/lab.c
+++ 9/lab.c
@@ -134,7 +134,7 @@ int main(int argc, char* argv[]){
 
     for (int i = 0; i < COUNT_OF_PHILOSOPHERS; i++){
         if(0 != pthread_mutex_destroy(&forks[i])){
-            fprintf(stderr, "pthread_mutex_destroy error (%d mutex)\n", i);
+            fprintf(stderr, "pthread_mutex_destroy error, %d mutex\n", i);
         }
     }
 
